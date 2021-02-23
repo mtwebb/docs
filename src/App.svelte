f690b2eae88e3e1a6572c306d199ab37cac4fe63
Nicolo John Davis <nicolodavis@gmail.com>
2021-02-23 10:36:39 +0800

    css tweak

--- src/App.svelte
+++ src/App.svelte
@@ -46,11 +46,11 @@
     @apply flex flex-row items-start mb-2 p-2;
 
     .circle {
-      @apply rounded-full flex-shrink-0 bg-gray-600 mr-2 text-white font-bold w-8 h-8 flex items-center justify-center;
+      @apply cursor-pointer shadow transition rounded-full flex-shrink-0 bg-gray-800 mr-2 text-white font-bold w-8 h-8 flex items-center justify-center;
     }
 
     &.active .circle {
-      @apply bg-black;
+      @apply bg-primary;
     }
 
     .content {
